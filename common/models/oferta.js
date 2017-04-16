ca2b4af7cf9c41b9f50596bf42b6fc8e79b47cdf
Antonio Armero <antonioarmeroperez@gmail.com>
2017-04-16 13:07:45 +0200

    Metodo Remoto de Nº de demandantes Inscritos

--- common/models/oferta.js
+++ common/models/oferta.js
@@ -1,5 +1,46 @@
 'use strict';
+var config = require('../../server/config.local.js');
+var path = require('path');
+var app = require('../../server/server.js');
 
 module.exports = function(Oferta) {
+
+
+
+	Oferta.usuariosInscritos = function(id,cb){
+			var Inscrito = app.models.Inscrito;
+				
+				Inscrito.count( {ofertaId: id}, function(err, count) {
+		      		if (err) return cb(err);
+		      		return cb(null,("El número de demandantes de empleo en esta oferta es de " + count));
+		      	});
+
+
+
+
+		};
+
+
+Oferta.remoteMethod(
+		'usuariosInscritos', {
+			description: 'Devuelve el número de demandantes inscritos en la oferta',
+			accepts: [{
+					arg: 'id',
+				type: 'integer',
+				required: true
+			}],
+			returns: {
+				arg: 'msg',
+				type: 'string'
+			},
+			http: {
+				path: '/:id/usuariosInscritos',
+				verb: 'get'
+			},
+		}
+);
+
+
+
 
 };
