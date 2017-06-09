3c424c509190abbceb4101e839743e229f8441d3
Antonio Armero <antonioarmeroperez@gmail.com>
2017-06-09 20:35:39 +0200

    bug 3

--- client/js/desarrollojquery.js
+++ client/js/desarrollojquery.js
@@ -1,21 +1,7 @@
 $(document).ready(function(){
-	$('#about').addClass('visible');
-$('#menulogin').on('click',function(){
-	if($('#about').hasClass('visible')){
-		
-		$('#about').removeClass('visible');
-		$('#about').addClass('oculto');
-	}else{
-		$('#about').removeClass('oculto');
-		$('#about').addClass('visible');
-	}
-
-});
 
    
     loadProfile();
-
-$('#correosolicita').attr('href','mailto://'+correo+'?subject=Registrar%20Empresa');
 
 });
 
